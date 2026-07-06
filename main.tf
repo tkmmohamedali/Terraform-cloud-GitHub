da02943046a416c072f0d34add3121e509ef7ec7
tkmmohamedali <tkmmohamedali@gmail.com>
2026-07-06 09:14:07 +0530

    version, ami id changed by ali

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.15.7" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 6.0"
      }
     }
    }
@@ -14,6 +14,6 @@ provider "aws" {
 	}
 
 resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+	ami = "ami-06067086cf86c58e6" 
 	instance_type = "t2.micro"
 }
